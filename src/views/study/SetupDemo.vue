593462e7437d08a5e50ad4c2d5bf5a6fbb0fccc6
Tianqing Liu <liutq@hollycrm.com>
2023-08-07 19:05:45 +0800

    init project

---
+++ src/views/study/SetupDemo.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="setup-demo">
+    姓名：<span>{{ name }}</span>
+    年龄：<span>{{ age }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SetupDemo',
+  setup() {
+    let name = '张三';
+    let age = 18;
+
+    return {
+      name,
+      age,
+    }
+  }
+}
+</script>
